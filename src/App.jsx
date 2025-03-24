e1c09da497bd8d4bc6070d64f23e5113e7c73177
Ghulam Saqlain <saqlainghulam78@gmail.com>
2025-03-24 11:54:49 +0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+// import Header from './components/Header';
+// import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
+import Search from './pages/Search';
+
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
+
+
+      </Routes>
+
+    </BrowserRouter>
+
   )
 }
